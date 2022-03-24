445cef4ee810f453faa5003a3db0833754dbd9ca
elishadiah <elishadiah326@gmail.com>
2022-03-24 12:53:30 -0400

    complete sidebar design

--- src/components/SiderBar/Sidebar.jsx
+++ src/components/SiderBar/Sidebar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Divider } from "./Divider";
 import { Layer } from "./Layer";
 import { NewLayer } from "./NewLayer";
 
@@ -19,7 +20,34 @@ const layerData = [
     rarity: 20,
   },
 ];
+const priceData = [
+  {
+    title: "100 Generative Art",
+    price: "0.00",
+    link: "100",
+  },
+  {
+    title: "1000 Generative Art",
+    price: "179.00",
+    link: "1000",
+  },
+  {
+    title: "5000 Generative Art",
+    price: "279.00",
+    link: "5000",
+  },
+  {
+    title: "10000 Generative Art",
+    price: "389.00",
+    link: "10000",
+  },
+];
 export const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const generateCollection = (item) => {
+    console.log("Generate Function: ", item);
+  };
   return (
     <div className="sidebar">
       <div className="sidebar_title">Layers</div>
@@ -29,8 +57,34 @@ export const Sidebar = () => {
       <NewLayer />
       <div className="buttons">
         <button>Preview</button>
-        <button>Pay & Generate Col.</button>
+        <button onClick={() => setIsOpen(!isOpen)}>Pay & Generate Col.</button>
       </div>
+      {isOpen && (
+        <div className="generate_collection">
+          <p className="collection_size">Collection Size:</p>
+          <Divider />
+          <div className="generate_main">
+            {priceData.map((item) => (
+              <div
+                className="generate_item"
+                onClick={() => generateCollection(item.link)}
+              >
+                <p>{item.title}</p>
+                <p>(${item.price})</p>
+              </div>
+            ))}
+          </div>
+          <Divider />
+          <p className="generate_item">
+            My Collection is smaller or equal to 100
+          </p>
+          <Divider />
+          <p>
+            Want another size? <br /> Select a number up to 10,000 in the
+            right-hand column of your scren.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
