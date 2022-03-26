0cd215321b63c7274e005f1a7754130a09d6d5a2
elishadiah <elishadiah326@gmail.com>
2022-03-26 10:06:10 -0400

    update properties place holder

--- src/components/PropertyManager/ProjectProperty.jsx
+++ src/components/PropertyManager/ProjectProperty.jsx
@@ -5,21 +5,21 @@ export const ProjectProperty = () => {
     <div className="property_container">
       <div className="property_item">
         <p>Project Name</p>
-        <input type="text" placeholder="Project" />
+        <input type="text" placeholder="Project Name" />
       </div>
       <div className="property_item">
         <p>Project Description</p>
-        <input type="text" placeholder="Project" />
+        <input type="text" placeholder="Project Description" />
       </div>
       <div className="property_item">
         <p>Collection Size</p>
-        <input type="text" placeholder="Project" />
+        <input type="text" placeholder="Collection Size" />
       </div>
       <div className="property_item">
         <p>Dimesion</p>
         <div className="property_dimension">
-          <input type="text" placeholder="Project" />
-          <input type="text" placeholder="Project" />
+          <input type="text" placeholder="" />
+          <input type="text" placeholder="" />
         </div>
         <button className="export_button">Export to: webp</button>
       </div>
